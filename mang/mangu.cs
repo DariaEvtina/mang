0c266478fe74c3b10410843152c26add7e4b7fde
mangle12369zzaa@gmail.com <mangle12369zzaa@gmail.com>
2022-09-09 11:06:29 +0300

    ggnf

--- mang/mangu.cs
+++ mang/mangu.cs
@@ -9,21 +9,10 @@ namespace mang
 {
     internal class mangu : IComparable<int>
     {
-        public List<tegelane> tegelansed = new List<tegelane>() { };
+        public tegelane[] tegelansed = new tegelane[] { };
         public mangu()
         {
-            tegelane pea_tegelane = new tegelane("peategenale");
-            pea_tegelane.lisaEse("Tavaline haldjarüü", 50);
-            pea_tegelane.lisaEse("Võlukeep", 1000);
-            tegelansed.Add(pea_tegelane);
-            tegelane teine_tegelane = new tegelane("teinetegenale");
-            teine_tegelane.lisaEse("Tavaline haldjarüü", 50);
-            tegelansed.Add(teine_tegelane);
-            tegelane kolme_tegelane = new tegelane("kolmetegenale");
-            kolme_tegelane.lisaEse("Väikeste haavade ravi jook", 200); 
-            kolme_tegelane.lisaEse("Väga suure osavuse kindad", 600);
-            kolme_tegelane.lisaEse("Külmavastane ripats", 400);
-            tegelansed.Add(kolme_tegelane);
+            
         }
 
         public int CompareTo(int other)
@@ -36,7 +25,7 @@ namespace mang
                 foreach (var item in tegelansed)
                 {
                     Console.WriteLine(item.info() + "  " + CompareTo(item.eseSum()));
-                }
+                }            
         }
         public void suurimaPunktideArvuga()
         {
--- mang/peaclass.cs
+++ mang/peaclass.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace mang
 {
@@ -10,7 +12,23 @@ namespace mang
     {
         public static void loeEsemed(string filenimi)
         {
-
+            List<Ese> esemed= new List<Ese> { };
+            string line;
+            using (StreamReader sr = new StreamReader(new FileStream(filenimi, FileMode.OpenOrCreate)))
+            {
+                while (sr.ReadLine() == null)
+                {
+                    if ((line=sr.ReadLine()) != null)
+                    {
+                        string[] rida = line.Split(';');
+                        esemed.Add(new Ese(rida[0], Convert.ToInt32(rida[1])));
+                    }
+                }
+            }
+        }
+        public peaclass()
+        {
+            mangu mangi= new mangu();
         }
     }
 }
